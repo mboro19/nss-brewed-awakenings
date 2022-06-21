79ecb9bee0a0c83fb037884de2d26af96e23cb90
David Stewart <mboro19@gmail.com>
2022-06-21 13:58:15 -0500

    final commit, added alerts for sales and prices

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -6,6 +6,77 @@ const employees = getEmployees()
 const orders = getOrders()
 
 
+//add an event listener to return a window alert showing the price of what the user clicked.
+
+
+document.addEventListener(
+    `click`,
+    (clickEvent) => {
+
+        const itemClicked = clickEvent.target;
+
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+                
+                for(const product of products){
+
+                    if(product.id === parseInt(productId)){
+
+            
+                    window.alert(`"A ${product.name} costs just $${product.price}."`)
+                }}}
+            }
+);
+
+document.addEventListener(
+    `click`,
+    (clickEvent) => {
+
+        const itemClicked = clickEvent.target;
+
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+                
+                for(const employee of employees){
+
+                    if(employee.id === parseInt(employeeId)){
+
+            
+                    window.alert(`"${employee.name} has sold ${employee.sales} product(s)."`)
+                }}}
+            }
+);
+
+
+//create a function that will determine how many sales each employee has, and will add a property to their objects for sales...
+
+
+const salesFunc = () => {
+
+for(let employee of employees){
+    
+    employee.sales = 0;
+
+    for(let order of orders){
+
+        if(order.employeeId === employee.id){
+
+              employee.sales++
+            
+        }
+    }
+}
+    
+    return employees.sales
+
+}
+
+salesFunc();
+
+
+
+
+
 // Function whose responsibility is to find the product for an order
 const findProduct = (order, allProducts) => {
 
